774f7f30497b71676d0480218376a2c8e5cfb6ca
EgoShish <flipflopy@mail.ru>
2026-03-25 23:51:20 +0300

    проект готов, нужно потестить
    Добавил рисование графиков

--- Database.cs
+++ Database.cs
@@ -67,5 +67,35 @@ namespace GraphicsPanelStanki
             this.CloseConnection();
             return table;
         }
+        public DataTable GetMachineLoadHistory(string name)
+        {
+            DataTable table = new DataTable();
+            this.OpenConnection();
+
+            MySqlCommand command = new MySqlCommand("GetMachineLoadHistory", connection);
+            command.CommandType = CommandType.StoredProcedure;
+            command.Parameters.AddWithValue("@target_name", name);
+            adapter.SelectCommand = command;
+            adapter.Fill(table);
+
+            this.CloseConnection();
+            return table;
+        }
+        public DataTable GetMachineLoadHistoryByPeriod(string name, DateTime dt1, DateTime dt2)
+        {
+            DataTable table = new DataTable();
+            this.OpenConnection();
+
+            MySqlCommand command = new MySqlCommand("GetMachineLoadHistoryByPeriod", connection);
+            command.CommandType = CommandType.StoredProcedure;
+            command.Parameters.AddWithValue("@target_name", name);
+            command.Parameters.AddWithValue("@t1", dt1);
+            command.Parameters.AddWithValue("@t2", dt2);
+            adapter.SelectCommand = command;
+            adapter.Fill(table);
+
+            this.CloseConnection();
+            return table;
+        }
     }
 }
