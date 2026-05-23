227eee35d953a23239b1b7a4bd8a7015ce4db2f0
Student <student@stankin.local>
2026-05-23 15:28:50 +0300

    wpf update

---
+++ WpfApp1/Database.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace WpfApp1
+{
+    public class Database
+    {
+        private MySqlDataAdapter adapter;
+        private MySqlConnection connection;
+
+        public Database()
+        {
+            connection = new MySqlConnection("Server=localhost; Database=stankidb; User ID=root; Password=root");
+            adapter = new MySqlDataAdapter();
+        }
+        public void OpenConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Closed) { connection.Open(); }
+        }
+        public void CloseConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Open) { connection.Close(); }
+        }
+        public DataTable GetTypesList()
+        {
+            DataTable table = new DataTable();
+            this.OpenConnection();
+
+            MySqlCommand command = new MySqlCommand("GetTypesList", connection);
+            command.CommandType = CommandType.StoredProcedure;
+            adapter.SelectCommand = command;
+            adapter.Fill(table);
+
+            this.CloseConnection();
+            return table;
+        }
+        public DataTable GetModelsByTypeName(string name)
+        {
+            DataTable table = new DataTable();
+            this.OpenConnection();
+
+            MySqlCommand command = new MySqlCommand("GetModelsByTypeName", connection);
+            command.CommandType = CommandType.StoredProcedure;
+            command.Parameters.AddWithValue("@target_typename", name);
+            adapter.SelectCommand = command;
+            adapter.Fill(table);
+
+            this.CloseConnection();
+            return table;
+        }
+        public DataTable GetMachineStatusLogs(string name)
+        {
+            DataTable table = new DataTable();
+            this.OpenConnection();
+
+            MySqlCommand command = new MySqlCommand("GetMachineStatusLogs", connection);
+            command.CommandType = CommandType.StoredProcedure;
+            command.Parameters.AddWithValue("@target_name", name);
+            adapter.SelectCommand = command;
+            adapter.Fill(table);
+
+            this.CloseConnection();
+            return table;
+        }
+        public DataTable GetMachineLoadHistory(string name)
+        {
+            DataTable table = new DataTable();
+            this.OpenConnection();
+
+            MySqlCommand command = new MySqlCommand("GetMachineLoadHistory", connection);
+            command.CommandType = CommandType.StoredProcedure;
+            command.Parameters.AddWithValue("@target_name", name);
+            adapter.SelectCommand = command;
+            adapter.Fill(table);
+
+            this.CloseConnection();
+            return table;
+        }
+        public DataTable GetMachineLoadHistoryByPeriod(string name, DateTime dt1, DateTime dt2)
+        {
+            DataTable table = new DataTable();
+            this.OpenConnection();
+
+            MySqlCommand command = new MySqlCommand("GetMachineLoadHistoryByPeriod", connection);
+            command.CommandType = CommandType.StoredProcedure;
+            command.Parameters.AddWithValue("@target_name", name);
+            command.Parameters.AddWithValue("@t1", dt1);
+            command.Parameters.AddWithValue("@t2", dt2);
+            adapter.SelectCommand = command;
+            adapter.Fill(table);
+
+            this.CloseConnection();
+            return table;
+        }
+    }
+}
+}
--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,28 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Database DB;
+        private string[] headerText;
         public MainWindow()
         {
             InitializeComponent();
+            DB = new Database();
+            headerText = new string[2];
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Ваш код из WinForms работает и здесь
+            var table = DB.GetTypesList();
+            foreach (DataRow t in table.Rows)
+            {
+                cmbbx_typeOfMachine.Items.Add((string)(t.ItemArray[1]));
+            }
+
+            if (cmbbx_typeOfMachine.Items.Count > 0)
+            {
+                cmbbx_typeOfMachine.SelectedIndex = 0;
+            }
         }
 
         private void MyButton_Click(object sender, RoutedEventArgs e)
@@ -38,5 +58,19 @@ namespace WpfApp1
                 MessageBox.Show($"{selectedText}");
             }
         }
+
+        private void MyComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (cmbbx_typeOfMachine.SelectedItem is ComboBoxItem selectedItem)
+            {
+                // Значение сохраняется в переменную
+                string selectedParam = selectedItem.Content.ToString();
+
+                // Здесь можно сразу вызвать метод, использующий эту переменную
+                System.Diagnostics.Debug.WriteLine($"Выбрано: {selectedParam}");
+            }
+        }
+
+
     }
 }
